1d90f1165a4d86dcefe8701e7af09b026ddf2024
Adarsh Rathore <adarshrathore@s.amity.edu>
2023-07-05 16:24:00 +0530

    not divisible series

--- not_divisible_series.cpp
+++ not_divisible_series.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int terms,num;
+    int terms,num,temp;
     cin>>terms;
     cin>>num;
-    for(int i=0;)
+    int i=0;
+    while(true){
+        if(terms==0){
+            break;
+        }
+        temp=(3*i)+2;
+        if(temp % num != 0 && temp > num){
+            cout<<temp<<"\n";
+            terms--;
+        }
+        i++;
+    }
 }
